5ccb54fbe0373bdec0529162bb3a653972e0c8bf
Yiang <1712064922@qq.com>
2022-10-31 15:14:06 +0800

    医院设置removeById，解决跨域请求问题

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -3,13 +3,17 @@ package com.atguigu.yygh.hosp.controller;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 //医院设置接口
+@CrossOrigin//设置跨域请求
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -21,5 +25,11 @@ public class HospitalSetController {
 	public List<HospitalSet> findAll() {
 		List<HospitalSet> list = hospitalSetService.list();
 		return list;
+	}
+
+	//逻辑删除
+	@DeleteMapping("{id}")
+	private boolean removeById(@PathVariable String id) {
+		return hospitalSetService.removeById(id);
 	}
 }
