0bec59cd35c5799c921cf2bc14ab036e0ad78792
Yiang <1712064922@qq.com>
2022-11-05 09:42:08 +0800

    搭建数据字典模块

---
+++ service/service-cmn/src/main/java/com/atguigu/yygh/cmn/config/HospConfig.java
@@ -0,0 +1,20 @@
+package com.atguigu.yygh.cmn.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.atguigu.yygh.cmn.mapper")
+public class HospConfig {
+	/*
+	 * 分页插件
+	 */
+	@Bean
+	public PaginationInterceptor paginationInterceptor() {
+		return new PaginationInterceptor();
+	}
+}
