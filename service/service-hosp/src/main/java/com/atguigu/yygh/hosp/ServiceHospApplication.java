42bb9696f6364fd09744eea1d80184046a8a5799
Yiang <1712064922@qq.com>
2022-11-09 09:52:09 +0800

    引入nacos

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,11 +2,13 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.atguigu"})//扫描到common模块包下的swagger2配置类
 //@MapperScan(basePackages = {"com.atguigu.yygh.hosp.mapper"})
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceHospApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHospApplication.class, args);
